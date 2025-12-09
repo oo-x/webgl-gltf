eb74a1f2e57c8b8561fe3635f4156047556e87a5
null <null>
2025-12-09 21:09:23 +0800

    multicanvas

--- src/gl/context.js
+++ src/gl/context.js
@@ -4,23 +4,34 @@ let gl = null
 /** @type {HTMLCanvasElement | null} */
 let canvas = null
 
+/** @type {WeakMap<HTMLCanvasElement, WebGL2RenderingContext>} */
+const contexts = new WeakMap()
+
 const onContextLost = () => {
 	gl = null
 }
 
-/** @param {HTMLCanvasElement | string} c */
+/** @param {HTMLCanvasElement | null | string} c */
 export function setCanvas(c) {
-	if (typeof c === 'string') {
-		canvas = document.querySelector(c)
-	} else {
-		canvas = c
+	const el = typeof c === 'string' ? document.querySelector(c) : c
+	if (!el) throw new Error('canvas not found')
+	if (!(el instanceof HTMLCanvasElement)) throw new Error('invalid canvas')
+
+	el.addEventListener('webglcontextlost', onContextLost, false)
+
+	if (canvas && gl) {
+		// we have an earlier canvas, stash the gl
+		contexts.set(canvas, gl)
 	}
-	canvas.addEventListener('webglcontextlost', onContextLost, false)
+
+	gl = null
+	canvas = el
+	return canvas
 }
 
 export function getGl() {
 	if (gl) return gl
-	const gl2 = canvas?.getContext('webgl2')
+	const gl2 = contexts.get(canvas) ?? canvas?.getContext('webgl2')
 	if (!gl2) throw new Error('could not getContext')
 	gl = gl2
 	return gl
